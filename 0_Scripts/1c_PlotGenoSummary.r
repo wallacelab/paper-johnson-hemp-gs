23e1abbe212fb2e2ff69f2094104dbd3eee91d2c
Jason G. Wallace <jason.wallace@uga.edu>
2021-01-21 09:42:33 -0500

    Include normalize genotype depth in plots

--- 0_Scripts/1c_PlotGenoSummary.r
+++ 0_Scripts/1c_PlotGenoSummary.r
@@ -14,11 +14,14 @@ parser$add_argument("-s", "--sitefile", help="TASSEL genotype summary file of si
 parser$add_argument("-t", "--taxafile", help="TASSEL genotype summary file of taxa")
 parser$add_argument("-d", "--depthfile", help="VCFTools depth output file")
 parser$add_argument("-g", "--genodepth", help="VCFTools output of genotype depth")
+parser$add_argument("-r", "--readdepth", help="LGC genomics file of sample read depths")
 parser$add_argument("-o", "--outfile", help="Output graphic")
 parser$add_argument("-m", "--max-datapoints", type="integer", help="Randomly subset to this many datapoints if dataset has more")
 args=parser$parse_args()
-# setwd('/home/jgwall/Projects/PearlMillet/BoubacarPearlMilletPops/GenomeLinkageMaps/2_Analysis/')
-# args=parser$parse_args(c("-s","2a_sitesummary.txt", "-t", "2a_taxasummary.txt", "-d", "2a_depth.txt", "-o", "99_tmp.png"))
+# setwd('/home/jgwall/Projects/Hemp/HempAnalysis_JasonRedo/1a_filtering/')
+# args=parser$parse_args(c("-s","1c_sitesummary.txt", "-t", "1c_taxasummary.txt", 
+#                          "-d", "1c_site_depth.txt", "-g", "1c_geno_depth.txt.gz", 
+#                          "-r", "../0_sample_read_counts.tsv", "-o", "99_tmp.png"))
 
 
 plots = list()
@@ -54,12 +57,55 @@ if(!is.null(args$depthfile)){
 if(!is.null(args$genodepth)){
     
     cat("Processing genotype depth data from",args$genodepth, "\n")
-    genodepth = scan(args$genodepth) #read in 
+    cat("\t(Note: this could take a while)\n")
+    
+    # Use header to determine number of samples to speed reading in
+    header=scan(args$genodepth, nline=1, what=character())
+    ntaxa = length(header)-2 # Take off 2 for CHROM, POS 
+    
+    # Read in file
+    colClasses = c("NULL", "NULL", rep("numeric", ntaxa)) #Specifying column classes speeds things up
+    genodepth = read.delim(args$genodepth, header=TRUE, colClasses = colClasses)
+    genodepth = as.matrix(genodepth) # Also to speed things up
+    colnames(genodepth) = sub(colnames(genodepth), pattern=".+(P..HDGS.[0-9]+).+", repl="\\1")
+    colnames(genodepth) = gsub(colnames(genodepth), pattern=".", repl="-", fixed=T)
+    
+    # Normalize to read depth per sample
+    sample_depths = read.delim(args$readdepth, row.names=1)
+    read_depth = sample_depths[colnames(genodepth),"Restriction.enzyme.filtered.read.pairs"]
+    norm_factors = mean(read_depth) / read_depth
+    genodepth_norm = t(t(genodepth) * norm_factors)
+    
+    # Binplot of depths
+    depthstats = data.frame(total_depth = rowSums(genodepth),
+                            mean_depth = apply(genodepth, FUN=mean, MARGIN=1),
+                            sd_depth = apply(genodepth, FUN=sd, MARGIN=1),
+                            total_depth_norm = rowSums(genodepth_norm),
+                            mean_depth_norm = apply(genodepth_norm, FUN=mean, MARGIN=1),
+                            sd_depth_norm = apply(genodepth_norm, FUN=sd, MARGIN=1),
+                            n_present = rowSums(genodepth > 0))
+    depthstats$fraction_present = depthstats$n_present / ntaxa
+    
+    ## Binplot - Everything
+    bindepth = ggplot(depthstats, mapping=aes(x=log10(mean_depth_norm), y=fraction_present)) +
+        geom_bin2d() +
+        ggtitle("Mean depth vs faction present (all)") +
+        scale_fill_gradient(low='blue', high='red')
+    
+    # Binplot - Only things with at least 0.5 mean depth
+    substats = subset(depthstats, depthstats$mean_depth >= 0.5)
+    ggplot(substats, mapping=aes(x=log10(mean_depth_norm), y=fraction_present)) +
+        geom_bin2d() +
+        ggtitle("Mean depth vs faction present (mean >= 0.5)")+
+        scale_fill_gradient(low='blue', high='red')
+    
+    
     if(!is.null(args$max_datapoints) && length(genodepth) > args$max_datapoints){ # Subset if needed
         cat("\tRandomly subsetting to",args$max_datapoints,"data points\n")
         genodepth = sample(genodepth, args$max_datapoints, replace=F)
     }
     genodepth = sort(genodepth) # Sort for plotting
+    
     
     # Depth - TODO: As histogram or as dot plot? (Prob dots to match % het)
     genodepth_raw = qplot(1:length(genodepth), genodepth, color=I('deeppink')) +
